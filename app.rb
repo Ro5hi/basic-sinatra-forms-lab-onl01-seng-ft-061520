08cdc31996fa9d3907695d7b160f8e599d4e0a89
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-16 16:22:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,21 @@ require 'sinatra/base'
 class App < Sinatra::Base
   
   get '/newteam' do 
+    
     erb :newteam 
+    
   end 
   
   post '/team' do 
+    
     @name = params[:name]
     @coach = params[:coach]
-    @pointguard = params[:pointguard]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @c = params[:c]
+    
+    erb :
     
   end 
 
